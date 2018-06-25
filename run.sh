7a38f59d1d994e7d885223673e827f91c751f43b
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2018-06-25 17:04:29 -0300

    Usa `rows csv2sqlite` para conversão de dados

--- run.sh
+++ run.sh
@@ -2,11 +2,19 @@
 
 set -e
 
+cd /app
+
+DOWNLOAD_SCRIPT=data/download.sh
+EMPRESAS=data/output/empresas.csv.xz
+HOLDINGS=data/output/holdings.csv.xz
+SOCIOS=data/output/socios.csv.xz
+DATABASE=data/output/socios-brasil.sqlite
+
 time python3 socios.py create-download-script
-time sh download.sh
+time sh $DOWNLOAD_SCRIPT
 time python3 socios.py convert-all
 time python3 socios.py merge-partner-files
 time python3 socios.py fix-partner-file
 time python3 socios.py extract-companies
-time python3 socios.py extract-company-company-partnerships
-time python3 csv2sqlite.py
+time python3 socios.py extract-holdings
+time rows csv2sqlite $EMPRESAS $HOLDINGS $SOCIOS $DATABASE
