8060df2f8147929a67416725aac8074993a034cc
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2019-10-30 19:27:29 -0300

    Reorganiza importação para o PostgreSQL

--- import-postgresql.sh
+++ import-postgresql.sh
@@ -3,18 +3,33 @@
 SCHEMA_PATH="schema"
 OUTPUT_PATH="data/output"
 
-#time rows pgimport --schema="$SCHEMA_PATH/empresa.csv" "$OUTPUT_PATH/empresa.csv.gz" "$POSTGRESQL_URI" empresa
-#time rows pgimport --schema="$SCHEMA_PATH/socio.csv" "$OUTPUT_PATH/empresa-socia.csv.gz" "$POSTGRESQL_URI" empresa_socia
-#time rows pgimport --schema="$SCHEMA_PATH/socio.csv" "$OUTPUT_PATH/socio.csv.gz" "$POSTGRESQL_URI" socio
-#time rows pgimport --schema="$SCHEMA_PATH/cnae-secundaria.csv" "$OUTPUT_PATH/cnae-secundaria.csv.gz" "$POSTGRESQL_URI" cnae_secundaria
+function import_table() {
+	filename="$1"
+	schema="$2"
+	tablename="$3"
 
+	time rows pgimport \
+		--schema="$SCHEMA_PATH/${schema}.csv" \
+		"$OUTPUT_PATH/${filename}.csv.gz" \
+		"$POSTGRESQL_URI" \
+		"$tablename"
+}
+
+# Import main tables
+import_table empresa empresa empresa
+import_table socio socio socio
+import_table empresa-socia socio empresa_socia
+import_table cnae-secundaria cnae-secundaria cnae_secundaria
+
+# Import CNAE tables
 schema="$SCHEMA_PATH/cnae.csv"
 for filename in $OUTPUT_PATH/cnae-1*.csv* $OUTPUT_PATH/cnae-2*.csv*; do
     versao="$(basename $filename | sed 's/.csv.*//; s/cnae-//; s/\.//')"
-    time rows pgimport --schema="$schema" "$filename" "$POSTGRESQL_URI" "cnae_$versao"
+    import_table $filename $schema "cnae_$versao"
 done
 
+# Execute SQL queries
 for filename in sql/*.sql; do
-    echo $filename
-    #time cat $filename | psql $POSTGRESQL_URI
+    echo "Executing ${filename}..."
+    time cat $filename | psql $POSTGRESQL_URI
 done
--- run.sh
+++ run.sh
@@ -4,26 +4,54 @@ set -e
 
 mkdir -p data/download data/output
 
-CONNECTIONS=4
-DOWNLOAD_URL="https://receita.economia.gov.br/orientacao/tributaria/cadastros/cadastro-nacional-de-pessoas-juridicas-cnpj/dados-publicos-cnpj"
-FILE_URLS=$(wget --quiet --no-check-certificate -O - "$DOWNLOAD_URL" \
-	| grep --color=no DADOS_ABERTOS_CNPJ \
-	| grep --color=no ".zip" \
-	| sed 's/.*"http:/http:/; s/".*//' \
-	| sort)
-MIRROR_URL="https://data.brasil.io/mirror/socios-brasil"
 if [ "$1" = "--use-mirror" ]; then
 	USE_MIRROR=true
 else
 	USE_MIRROR=false
 fi
 
-for url in $FILE_URLS; do
-	if $USE_MIRROR; then
-		url="$MIRROR_URL/$(basename $url)"
-	fi
-	time aria2c --auto-file-renaming=false --continue=true -s $CONNECTIONS -x $CONNECTIONS --dir=data/download "$url"
-done
+function download_data() {
+	CONNECTIONS=4
+	DOWNLOAD_URL="https://receita.economia.gov.br/orientacao/tributaria/cadastros/cadastro-nacional-de-pessoas-juridicas-cnpj/dados-publicos-cnpj"
+	FILE_URLS=$(wget --quiet --no-check-certificate -O - "$DOWNLOAD_URL" \
+		| grep --color=no DADOS_ABERTOS_CNPJ \
+		| grep --color=no ".zip" \
+		| sed 's/.*"http:/http:/; s/".*//' \
+		| sort)
+	MIRROR_URL="https://data.brasil.io/mirror/socios-brasil"
 
-time python extract_dump.py data/output/ data/download/DADOS_ABERTOS_CNPJ*.zip
-time python extract_partner_companies.py data/output/socio.csv.gz data/output/empresa-socia.csv.gz
+	for url in $FILE_URLS; do
+		if $USE_MIRROR; then
+			url="$MIRROR_URL/$(basename $url)"
+		fi
+		time aria2c --auto-file-renaming=false --continue=true -s $CONNECTIONS -x $CONNECTIONS --dir=data/download "$url"
+	done
+}
+
+function extract_data() {
+	time python extract_dump.py data/output/ data/download/DADOS_ABERTOS_CNPJ*.zip
+}
+
+function extract_holdings() {
+	time python extract_holdings.py data/output/socio.csv.gz data/output/empresa-socia.csv.gz
+}
+
+function extract_cnae() {
+	for versao in "1.0" "1.1" "2.0" "2.1" "2.2" "2.3"; do
+		filename="data/output/cnae-$versao.csv"
+		rm -rf "$filename"
+		time scrapy runspider \
+			-s RETRY_HTTP_CODES="500,503,504,400,404,408" \
+			-s HTTPCACHE_ENABLED=true \
+			--loglevel=INFO \
+			-a versao="$versao" \
+			-o "$filename" \
+			cnae.py
+		gzip "$filename"
+	done
+}
+
+download_data
+extract_data
+extract_holdings
+extract_cnae
