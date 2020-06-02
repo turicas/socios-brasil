31215b51a48aa5a880be52bf708ee27b05b64748
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-06-02 19:30:59 -0300

    Altera arquivo que extrai holdings

--- extract_holdings.py
+++ extract_holdings.py
@@ -5,7 +5,15 @@ from rows.utils import CsvLazyDictWriter, open_compressed
 from tqdm import tqdm
 
 
-def filter_csv(input_filename, output_filename, filter_function, progress=True):
+def convert_row(row):
+    return {
+        "holding_cnpj": row["cnpj_cpf_do_socio"],
+        "holding_razao_social": row["nome_socio"],
+        "cnpj": row["cnpj"],
+    }
+
+
+def filter_csv(input_filename, output_filename, filter_function, convert_function, progress=True):
     fobj_reader = open_compressed(input_filename, mode="r")
     fobj_writer = open_compressed(output_filename, mode="w")
     csv_reader = DictReader(fobj_reader)
@@ -14,7 +22,7 @@ def filter_csv(input_filename, output_filename, filter_function, progress=True):
         csv_reader = tqdm(csv_reader)
     for row in csv_reader:
         if filter_function(row):
-            csv_writer.writerow(row)
+            csv_writer.writerow(convert_function(row))
     fobj_reader.close()
     fobj_writer.close()
 
@@ -29,4 +37,6 @@ if __name__ == "__main__":
         args.input_filename,
         args.output_filename,
         lambda row: row["identificador_de_socio"] == "1",
+        convert_function=convert_row,
+        progress=True,
     )
