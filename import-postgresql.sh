f1d60a9f9ccf33e6d1d0558a3e0821b6f12548a2
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-07-14 10:30:47 -0300

    Refatora script de importação

--- import-postgresql.sh
+++ import-postgresql.sh
@@ -16,22 +16,26 @@ function import_table() {
 		"$tablename"
 }
 
-# Import main tables
+function import_cnae_tables() {
+	schema="cnae"
+	for filename in $OUTPUT_PATH/cnae_1*.csv* $OUTPUT_PATH/cnae_2*.csv*; do
+		versao="$(basename $filename | sed 's/.csv.*//; s/cnae_//; s/\.//')"
+		filename=$(basename $filename | sed 's/.csv.gz//')
+		import_table $filename $schema "cnae_$versao"
+	done
+}
+
+function execute_sql_files() {
+	## Execute SQL queries in sql/*.sql
+	for filename in sql/*.sql; do
+		echo "Executing ${filename}..."
+		time cat $filename | psql $POSTGRESQL_URI
+	done
+}
+
 import_table empresa
 import_table cnae_cnpj
 import_table socio
 import_table holding
-
-# Import CNAE tables
-schema="cnae"
-for filename in $OUTPUT_PATH/cnae_1*.csv* $OUTPUT_PATH/cnae_2*.csv*; do
-    versao="$(basename $filename | sed 's/.csv.*//; s/cnae_//; s/\.//')"
-    filename=$(basename $filename | sed 's/.csv.gz//')
-    import_table $filename $schema "cnae_$versao"
-done
-
-## Execute SQL queries
-for filename in sql/*.sql; do
-    echo "Executing ${filename}..."
-    time cat $filename | psql $POSTGRESQL_URI
-done
+import_cnae_tables
+execute_sql_files
