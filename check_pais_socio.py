31460acce7373676b1d51dc94ce6595b1c03eb93
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-07-25 09:47:32 -0300

    Adiciona script para checar o país dos sócios

---
+++ check_pais_socio.py
@@ -0,0 +1,10 @@
+import csv
+from collections import Counter
+
+import rows
+from tqdm import tqdm
+
+
+reader = csv.DictReader(rows.utils.open_compressed("data/output/socio.csv.gz"))
+paises = Counter((row["codigo_pais"], row["nome_pais"]) for row in tqdm(reader))
+print(paises.most_common())
