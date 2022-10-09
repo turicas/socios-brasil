97ec688eff39b6bcdd66404278d6c2098abc1cc2
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2022-10-08 22:42:24 -0300

    Altera tabela de empresa/CNAE para estabelecimento/CNAE

--- sql/estabelecimento_cnae.sql
+++ sql/estabelecimento_cnae.sql
@@ -1,20 +1,20 @@
-DROP TABLE IF EXISTS empresa_cnae;
-CREATE TABLE empresa_cnae AS
+DROP TABLE IF EXISTS estabelecimento_cnae;
+CREATE TABLE estabelecimento_cnae AS
   SELECT
     e.cnpj,
     c.razao_social,
     e.nome_fantasia,
     e.cnae_principal,
     e.cnae_secundaria,
-    m.nome AS municipio,
-    e.codigo_municipio,
     e.uf,
+    e.codigo_municipio,
+    m.nome AS municipio,
     e.data_situacao_cadastral,
     e.codigo_situacao_cadastral,
     e.codigo_motivo_situacao_cadastral,
     e.data_inicio_atividade
-  FROM estabelecimento_uuid AS e
-    LEFT JOIN empresa_uuid AS c
-      ON e.empresa_uuid = c.empresa_uuid
+  FROM estabelecimento AS e
+    LEFT JOIN empresa AS c
+      ON e.empresa_uuid = c.uuid
     LEFT JOIN municipio_uf AS m
       ON m.codigo = e.codigo_municipio;
