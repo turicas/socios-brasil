09e1b7934312e64fae491ff2e56b80d5e8c896b9
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2022-11-10 00:43:37 -0300

    Adiciona pessoa_uuid em empresa e reordena colunas

--- sql/03-empresa.sql
+++ sql/03-empresa.sql
@@ -2,23 +2,35 @@ DROP TABLE IF EXISTS empresa;
 CREATE TABLE empresa AS
   SELECT
     e."uuid",
-    e.cnpj_raiz,
-    e.razao_social,
+    CASE
+      WHEN codigo_natureza_juridica = 2135 AND TRIM(razao_social) ~ ' [0-9.-]{11,14}$' THEN
+        person_uuid(
+          RIGHT(TRIM(razao_social), 11),
+          TRIM(REGEXP_REPLACE(TRIM(razao_social), '[,-]? ?(CPF)? ?(N ?|NO ?)?[:.-]? ?[0-9.-]{11,14}$', ' '))
+        )
+      ELSE NULL
+    END AS pessoa_uuid,
+
     e.codigo_natureza_juridica,
     e.codigo_qualificacao_responsavel,
-    e.capital_social,
     e.codigo_porte,
-    e.ente_responsavel_uf,
-    m.codigo AS ente_responsavel_codigo_municipio
+    m.codigo::smallint AS ente_responsavel_codigo_municipio,
+
+    e.capital_social,
+    e.cnpj_raiz,
+    e.razao_social,
+    e.ente_responsavel_uf
   FROM (
     SELECT
       company_uuid(cnpj_raiz || '000100') AS "uuid",
-      cnpj_raiz,
-      razao_social,
+
       codigo_natureza_juridica::smallint AS codigo_natureza_juridica,
       codigo_qualificacao_responsavel::smallint AS codigo_qualificacao_responsavel,
-      REPLACE(capital_social, ',', '.')::decimal AS capital_social,
       codigo_porte::smallint AS codigo_porte,
+
+      REPLACE(capital_social, ',', '.')::decimal AS capital_social,
+      cnpj_raiz,
+      razao_social,
       CASE
         WHEN ente_federativo IS NULL THEN NULL
         WHEN NOT ente_federativo LIKE '% - %' THEN sigla_uf(ente_federativo)
@@ -40,3 +52,4 @@ CREATE TABLE empresa AS
       ON e.ente_responsavel_uf = m.uf AND e.ente_responsavel_municipio = m.nome;
 
 CREATE INDEX idx_empresa_uuid ON empresa ("uuid");
+CREATE INDEX idx_empresa_mei ON empresa (pessoa_uuid);
